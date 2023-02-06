e79ba57122ed16d906ba8536b6bcafc005d9fac6
Diego Aldana <daldana9@gmail.com>
2023-02-06 15:54:55 -0300

    Created base Node project and Dockerfile

---
+++ README.md
@@ -0,0 +1,2 @@
+# Poke-Wiki-To-Notion
+Web Scraper to get information from Pokemon Wiki sites and add it to Notion
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(function(err, req, res, callback) {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+         res.status(403).send({
+             error: true,
+             message: "Wrong Body of json",
+             response: null
+         })
+          res.end()
+    }
+});
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Poke-Wiki-To-Notion 1.0.0 ' + new Date().toString());
+});
+
+app.listen(port, () => {
+    console.log('Poke-Wiki-To-Notion listening on port ' + port)
+});
