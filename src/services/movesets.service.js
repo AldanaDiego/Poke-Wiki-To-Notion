c875c620139d86c912e84c8d5e348f983daa4605
Diego Aldana <daldana9@gmail.com>
2023-02-27 15:36:59 -0300

    Moveset endpoint now includes details about learning methods

--- src/services/movesets.service.js
+++ src/services/movesets.service.js
@@ -6,16 +6,26 @@ async function getMoveset(pokemon, dexNumber) {
     var movesetUrl = process.env.SEREBII_WIKI_URL + '/pokedex-sv/' + pokemon;
     return axios.get(movesetUrl).then(resp => {
         if (resp.status == 200 && resp.data !== null && resp.data !== undefined) {
-            var moveset = [];
+            var moveset = {};
             try {
                 const dom = new JSDOM(resp.data).window.document;
                 const tables = dom.querySelectorAll("table");
                 tables.forEach(table => {
                     const tableTitle = table.querySelector("h3")?.textContent.trim();
-                    if (isTableTitleMoveset(tableTitle)) {
+                    const learningMethod = parseTableTitle(tableTitle);
+                    if (learningMethod !== null) {
                         const moves = table.querySelectorAll("a[href^='/attackdex']");
                         moves.forEach(move => {
-                            moveset.push(move.textContent.trim());
+                            moveName = move.textContent.trim();
+                            if (moveset[moveName] === undefined) {
+                                moveset[moveName] = {};
+                            }
+                            if (moveset[moveName][learningMethod] === undefined) {
+                                moveset[moveName][learningMethod] = [];
+                            }
+                            if (!moveset[moveName][learningMethod].includes("IX")) {
+                                moveset[moveName][learningMethod].push("IX");
+                            }
                         })
                         
                     }
@@ -31,17 +41,30 @@ async function getMoveset(pokemon, dexNumber) {
             return null;
         }
     }).catch(err => {
-        console.log('HUH' + err.message);
         console.log("Exception while getting moveset info: " + err.message | "undefined error");
         return null;
     });
 };
 
-function isTableTitleMoveset(tableTitle) {
-    if (tableTitle !== null && tableTitle !== undefined && (tableTitle.includes("Level Up") || tableTitle.includes("Attacks") || tableTitle.includes("Moves"))) {
-        return true;
+function parseTableTitle(tableTitle) {
+    if (tableTitle !== null && tableTitle !== undefined) {
+        if (tableTitle.includes("Level Up")) {
+            return "Level Up";
+        } else if (tableTitle.includes("Machine") || tableTitle.includes("Record") || tableTitle.includes("TM") ) {
+            return "TM";
+        } else if (tableTitle.includes("Egg")) {
+            return "Breeding";
+        } else if (tableTitle.includes("Tutor")) {
+            return "Tutor";
+        } else if (tableTitle.includes("Special")) {
+            return "Special Move";
+        } else if (tableTitle.includes("Transfer")) {
+            return "Transfer";
+        } else if (tableTitle.includes("Pre-Evolution")) {
+            return "Pre-Evolution";
+        }
     }
-    return false;
+    return null;
 }
 
 module.exports = {
