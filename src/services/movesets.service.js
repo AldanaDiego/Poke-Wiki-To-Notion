da90c348da14c1b898edb61ec7be4a5b17e27250
Diego Aldana <daldana9@gmail.com>
2023-03-01 10:20:26 -0300

    Moveset endpoint now includes details about moveset in each generation

--- src/services/movesets.service.js
+++ src/services/movesets.service.js
@@ -2,49 +2,88 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const axios = require('axios').default;
 
+const generationUrl = {
+    "I": "/pokedex/",
+    "II": "/pokedex-gs/",
+    "III": "/pokedex-rs/",
+    "IV": "/pokedex-dp/",
+    "V": "/pokedex-bw/",
+    "VI": "/pokedex-xy/",
+    "VII": "/pokedex-sm/",
+    "VIII": "/pokedex-swsh/",
+    "IX": "/pokedex-sv/",
+};
+
 async function getMoveset(pokemon, dexNumber) {
-    var movesetUrl = process.env.SEREBII_WIKI_URL + '/pokedex-sv/' + pokemon;
+    var generationMovesets = []
+    for (var generation in generationUrl) {
+        generationMovesets.push(getGenerationMoveset(pokemon, dexNumber, generation));
+    }
+    return Promise.all(generationMovesets).then(results => {
+        var mergedMoveset = {}
+        results.forEach(result => {
+            if (result != null) {
+                var generation = result['generation'];
+                var moveset = result['moveset'];
+                
+                for (var moveName in moveset) {
+                    var learningMethods = moveset[moveName];
+                    if (mergedMoveset[moveName] === undefined) {
+                        mergedMoveset[moveName] = {};
+                    }
+                    learningMethods.forEach(learningMethod => {
+                        if (mergedMoveset[moveName][learningMethod] === undefined) {
+                            mergedMoveset[moveName][learningMethod] = [];
+                        }
+                        mergedMoveset[moveName][learningMethod].push(generation);
+                    });
+                } 
+            }
+        });
+        
+        return mergedMoveset;
+    });
+};
+
+async function getGenerationMoveset(pokemon, dexNumber, generation) {
+    var movesetUrl = process.env.SEREBII_WIKI_URL + generationUrl[generation] + ((generation == "VIII" || generation == "IX") ? pokemon : (dexNumber + ".shtml"));
     return axios.get(movesetUrl).then(resp => {
         if (resp.status == 200 && resp.data !== null && resp.data !== undefined) {
-            var moveset = {};
             try {
+                var moveset = {};
                 const dom = new JSDOM(resp.data).window.document;
-                const tables = dom.querySelectorAll("table");
+                const tables = dom.querySelectorAll("table.dextable");
                 tables.forEach(table => {
-                    const tableTitle = table.querySelector("h3")?.textContent.trim();
+                    const tableTitle = table.querySelector("tr")?.textContent.trim();
                     const learningMethod = parseTableTitle(tableTitle);
                     if (learningMethod !== null) {
                         const moves = table.querySelectorAll("a[href^='/attackdex']");
                         moves.forEach(move => {
                             moveName = move.textContent.trim();
                             if (moveset[moveName] === undefined) {
-                                moveset[moveName] = {};
+                                moveset[moveName] = [];
                             }
-                            if (moveset[moveName][learningMethod] === undefined) {
-                                moveset[moveName][learningMethod] = [];
+                            if (!moveset[moveName].includes(learningMethod)) {
+                                moveset[moveName].push(learningMethod);
                             }
-                            if (!moveset[moveName][learningMethod].includes("IX")) {
-                                moveset[moveName][learningMethod].push("IX");
-                            }
-                        })
-                        
+                        });
                     }
                 });
-                return moveset;
-
+                
+                return { generation: generation, moveset: moveset };
             } catch (err) {
                 console.log("Exception while parsing moveset info " + err.message);
                 return null;
             }
         } else {
-            console.log("Status code when getting moveset info: " + resp.status);
+            console.log("Status code when getting moveset (Gen " + generation +") info: " + resp.status);
             return null;
         }
     }).catch(err => {
         console.log("Exception while getting moveset info: " + err.message | "undefined error");
         return null;
     });
-};
+}
 
 function parseTableTitle(tableTitle) {
     if (tableTitle !== null && tableTitle !== undefined) {
